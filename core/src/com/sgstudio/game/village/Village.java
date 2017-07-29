34f9709a850dca80f3c834b191c2b2acad841a05
pasha5174 <shebarshin00@gmail.com>
2017-07-29 13:19:42 +0300

    LOL2

--- core/src/com/sgstudio/game/village/Village.java
+++ core/src/com/sgstudio/game/village/Village.java
@@ -9,20 +9,20 @@ public class Village {
 	final int DEATH = 0;
 	final int FULHP = 10000;
 	int HP = 100000;
-	int hunger = 100;
-	int thirst = 100;
+	float hunger = 100f;
+	float thirst = 100f;
 	public void update(){
 		if(time!=(System.currentTimeMillis() - startTime) / 1000){
 			time++;
-			hunger -= 0.1;
-			thirst -= 0.3;
+			hunger -= 0.333333333333333333333333333333f;
+			thirst -= 0.416666666666666666666666666666f;
 			
 			
 			if (hunger < 0){
 				hunger = 0;
 			}
 			if (hunger == 0){
-				HP -= 0.5;
+				HP -= 300;
 			}
 			
 			
@@ -30,11 +30,19 @@ public class Village {
 				thirst = 0;
 			}
 			if (thirst == 0){
-				HP -= 1;
+				HP -= 400;
 			}
 			
+			if (HP < 0){
+				HP = 0;
+			}
+			/*if (thirst > 0){
 			System.out.println(hunger);
 			System.out.println(thirst);
+			} else {
+				System.out.println(hunger);
+				System.out.println(HP);
+			}*/
 		}
 	}
 }
