dba8617872dc0e6f0a3feaf335875e0f5a7127e7
YarikBur <yarikbur@Mail.ru>
2017-07-29 09:16:39 +0300

    Init

---
+++ desktop/src/com/sgstudio/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sgstudio.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sgstudio.game.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
