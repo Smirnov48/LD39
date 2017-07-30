198958763a5fee342f9db8b6e70eee57a8e881e5
pasha5174 <shebarshin00@gmail.com>
2017-07-30 09:56:58 +0300

    Иупшт ща вуауфе

--- core/src/com/sgstudio/game/Defeat.java
+++ core/src/com/sgstudio/game/Defeat.java
@@ -15,31 +15,26 @@ public class Defeat implements Screen{
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void render(float delta) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
