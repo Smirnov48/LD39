9c89c1240dc1cb17578f65b58698880b0a6278db
YarikBur <yarikbur@Mail.ru>
2017-07-30 17:04:33 +0300

    Auto stash before merge of "master" and "origin/master"

--- core/src/com/sgstudio/game/ground/Rails.java
+++ core/src/com/sgstudio/game/ground/Rails.java
@@ -1,29 +1,38 @@
 package com.sgstudio.game.ground;
 
+import java.util.Map;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.sgstudio.game.train.Train;
 import com.sgstudio.utils.Box2DHelper;
+import com.sgstudio.utils.Tiles;
 
 public class Rails {
+	private Tiles tiles;
 	Texture imgRails;
 	SpriteBatch batch;
 	int railsX;
-	final int railsY = 60;
+	final int railsY = 0;
 	private Train train;
 	private float speed = 0;
 	
 	private Sprite bgRails[] = {new Sprite(new Texture("rails1.png")), new Sprite(new Texture("rails1.png")), new Sprite(new Texture("rails1.png"))};
+	private Sprite front[] = {new Sprite(), new Sprite(),new Sprite()};
+	private Map<String, TextureRegion> atlasFront;
 	
 	public Rails(World world,SpriteBatch batch,Train train) {
+		tiles = new Tiles();
+		tiles.createAtlas("atlas/front.png", 3, 1);
+		atlasFront = tiles.getTextureRegion();
+		for(int i=0;i<3;i++) front[i] = new Sprite(atlasFront.get("tiles0_"+i));
 		this.train = train;
 		
 		Body ground = Box2DHelper.makeBox(world, new Vector2(Gdx.graphics.getWidth(), 68));
@@ -39,6 +48,8 @@ public class Rails {
 	public void render() {
 		for(int i = 0;i < 3; i++) bgRails[i].draw(batch);
 		for(int i=0;i<3;i++) bgRails[i].setX(bgRails[i].getX()-speed);
+		for(int i=0;i<3;i++) front[i].setX(bgRails[i].getX()-speed);
+		for(int i=0;i<3;i++) front[i].draw(batch);
 		
 		if(bgRails[0].getX()<=-800){
 			bgRails[0].setX(bgRails[2].getX()+bgRails[2].getWidth());
@@ -49,7 +60,6 @@ public class Rails {
 		if(bgRails[2].getX()<=-800){
 			bgRails[2].setX(bgRails[1].getX()+bgRails[1].getWidth());
 		}
-		
 	}
 	
 	public void update() {
