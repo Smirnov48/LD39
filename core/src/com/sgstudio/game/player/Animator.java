1bb24aaf3329c583b117f3b68d1db0cfef8ab490
Alexander Smirnov <smirnov48@gmail.com>
2017-07-31 16:52:13 +0300

    fix charter stop

--- core/src/com/sgstudio/game/player/Animator.java
+++ core/src/com/sgstudio/game/player/Animator.java
@@ -44,30 +44,34 @@ public class Animator {
 	        km = new KeyManager();
 	}
 	
+    boolean isLookLeft = false; 
     public void render() {
-    	boolean tr = true;
-		if (mainHero.getHeroX() > mainHero.getHeroX() - mainHero.getHeroDX()){
-	        stateTime += Gdx.graphics.getDeltaTime(); 
-	        currentFrame = (TextureRegion) walkAnimation.getKeyFrame(stateTime, true);
+		if (mainHero.getHeroX() > mainHero.getHeroX() - mainHero.getHeroDX()) {
+	        stateTime += Gdx.graphics.getDeltaTime();	        
+	        currentFrame = (TextureRegion) walkAnimation.getKeyFrame(stateTime, true);	        
 	        spriteBatch.draw(currentFrame, mainHero.getHeroX() - currentFrame.getRegionWidth()/2, mainHero.getHeroY() - currentFrame.getRegionHeight()/2,
 	        		currentFrame.getRegionWidth(), currentFrame.getRegionHeight()); 
-	        /*if(tr && (mainHero.getHeroX() == mainHero.getHeroX() - mainHero.getHeroDX())){
-				spriteBatch.draw(stop, mainHero.getHeroX() - stop.getWidth()/2, mainHero.getHeroY() - stop.getHeight()/2,
-		        		stop.getWidth(), stop.getHeight()); 
-			} */
+
+	    	if (mainHero.getHeroDX() > 0.0f && mainHero.getHeroDX() < 0.2f) {
+	    		isLookLeft = false;
+	    	}
 		} else if(mainHero.getHeroX() < mainHero.getHeroX() - mainHero.getHeroDX()){
 		    stateTime += Gdx.graphics.getDeltaTime(); 
 	        currentFrame = (TextureRegion) walkAnimation.getKeyFrame(stateTime, true);
 	        spriteBatch.draw(currentFrame, mainHero.getHeroX() + currentFrame.getRegionWidth()/2, mainHero.getHeroY() - currentFrame.getRegionHeight()/2,
 	        		-currentFrame.getRegionWidth(), currentFrame.getRegionHeight()); 
-	        tr = false;
-	        if(tr && !(mainHero.getHeroX() > mainHero.getHeroX() - mainHero.getHeroDX())){
-				spriteBatch.draw(stop, mainHero.getHeroX() + stop.getWidth()/2, mainHero.getHeroY() - stop.getHeight()/2,
-						-stop.getWidth(), stop.getHeight());
-			}
+
+	    	if (mainHero.getHeroDX() < 0.0f && mainHero.getHeroDX() > -0.2f) {
+	    		isLookLeft = true;
+	    	}
 		} else {
-			spriteBatch.draw(stop, mainHero.getHeroX() - stop.getWidth()/2, mainHero.getHeroY() - stop.getHeight()/2,
-	        		stop.getWidth(), stop.getHeight()); 
+			if (isLookLeft) {
+				spriteBatch.draw(stop, mainHero.getHeroX() + stop.getWidth()/2, mainHero.getHeroY() - stop.getHeight()/2,
+						-stop.getWidth(), stop.getHeight());				
+			} else {
+				spriteBatch.draw(stop, mainHero.getHeroX() - stop.getWidth()/2, mainHero.getHeroY() - stop.getHeight()/2,
+		        		stop.getWidth(), stop.getHeight());
+			}
 		}
     }
 }
