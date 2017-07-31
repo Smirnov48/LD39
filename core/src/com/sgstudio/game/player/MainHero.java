50b0f6f23623ac0813a1be4f723a53734d45c749
Alexander Smirnov <smirnov48@gmail.com>
2017-07-31 21:08:04 +0300

    hero circle

--- core/src/com/sgstudio/game/player/MainHero.java
+++ core/src/com/sgstudio/game/player/MainHero.java
@@ -97,7 +97,13 @@ public class MainHero {
 	}
 
 	private void createPhysics() {
-		body = Box2DHelper.makeBoxAroundSprite(world, sprite, "Player");
+		Vector2 size = new Vector2(sprite.getWidth(), sprite.getHeight());
+		Vector2 pos = new Vector2(0, -12);
+		body = Box2DHelper.makeCircle(world, size, pos, "Player");
+		size = new Vector2(sprite.getWidth() / 2, sprite.getHeight()/2 - 10);
+		pos = new Vector2(0, 10);
+		Box2DHelper.addShapeBox(body, size, pos, 1, true);
+		
 		Box2DHelper.setTransform(body, 290, 60, 0);
 		body.setFixedRotation(true);
 	}
