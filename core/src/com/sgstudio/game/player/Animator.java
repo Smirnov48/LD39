c74f706104dd5bbb78d984f5d4e84d8a1c91b980
pasha5174 <shebarshin00@gmail.com>
2017-07-31 15:50:20 +0300

    govno

--- core/src/com/sgstudio/game/player/Animator.java
+++ core/src/com/sgstudio/game/player/Animator.java
@@ -45,12 +45,13 @@ public class Animator {
 	}
 	
     public void render() {
+    	boolean tr = true;
 		if (mainHero.getHeroX() > mainHero.getHeroX() - mainHero.getHeroDX()){
 	        stateTime += Gdx.graphics.getDeltaTime(); 
 	        currentFrame = (TextureRegion) walkAnimation.getKeyFrame(stateTime, true);
 	        spriteBatch.draw(currentFrame, mainHero.getHeroX() - currentFrame.getRegionWidth()/2, mainHero.getHeroY() - currentFrame.getRegionHeight()/2,
 	        		currentFrame.getRegionWidth(), currentFrame.getRegionHeight()); 
-	        if(mainHero.getHeroDX() > 0){
+	        if(tr && (mainHero.getHeroX() == mainHero.getHeroX() - mainHero.getHeroDX())){
 				spriteBatch.draw(stop, mainHero.getHeroX() - stop.getWidth()/2, mainHero.getHeroY() - stop.getHeight()/2,
 		        		stop.getWidth(), stop.getHeight()); 
 			} 
@@ -59,7 +60,8 @@ public class Animator {
 	        currentFrame = (TextureRegion) walkAnimation.getKeyFrame(stateTime, true);
 	        spriteBatch.draw(currentFrame, mainHero.getHeroX() + currentFrame.getRegionWidth()/2, mainHero.getHeroY() - currentFrame.getRegionHeight()/2,
 	        		-currentFrame.getRegionWidth(), currentFrame.getRegionHeight()); 
-	        if(mainHero.getHeroDX() == 0){
+	        tr = false;
+	        if(tr && (mainHero.getHeroX() <= mainHero.getHeroX() - mainHero.getHeroDX())){
 				spriteBatch.draw(stop, mainHero.getHeroX() + stop.getWidth()/2, mainHero.getHeroY() - stop.getHeight()/2,
 						-stop.getWidth(), stop.getHeight());
 			}
